31d2b87268c3dbb3e0a67f73b34440557a0d26ba
Your Name <you@example.com>
2024-12-30 00:24:00 +0900

    your commit message

--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
-// /models/user.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true }, // 아이디 (예: 이메일)
-  name: { type: String, required: true }, // 이름
-  email: { type: String, required: true, unique: true }, // 이메일
-  password: { type: String, required: true }, // 비밀번호 (해시화된)
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    brokerage: { type: String, required: true },
+    accountNumber: { type: String, required: true },
+    password: { type: String, required: true },
+    balance: { type: Number, default: 0 },
+    stocks: { type: Array, default: [] }
 });
 
 module.exports = mongoose.model('User', userSchema);
