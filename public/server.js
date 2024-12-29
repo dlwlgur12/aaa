8e40657d5e3c7703235a4aea50b58d563525c1e9
Your Name <you@example.com>
2024-12-29 23:56:02 +0900

    your commit message

--- public/server.js
+++ public/server.js
@@ -91,12 +91,12 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({ userId: user._id, id: user.id, name: user.name }, SECRET_KEY, { expiresIn: '1h' });
 
     // 로그인 성공 후 사용자 이름과 잔고 정보 반환
-    res.json({
-      message: '로그인 성공',
-      token,
-      name: user.name,
-      balance: user.balance,
-      stocks: user.stocks,
+    res.json({ 
+      message: '로그인 성공', 
+      token, 
+      name: user.name, 
+      balance: user.balance, 
+      stocks: user.stocks 
     });
   } catch (error) {
     res.status(500).json({ message: '서버 오류: ' + error.message });
