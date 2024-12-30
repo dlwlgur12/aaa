fa5cc86a8638c7d1f478897ab4c78df701612fc3
Your Name <you@example.com>
2024-12-30 21:40:41 +0900

    JWT 인증 및 사용자 정보 API 수정

--- public/signup.js
+++ public/signup.js
@@ -18,7 +18,7 @@ document.getElementById('signup-form').addEventListener('submit', async function
 
     try {
         console.log('Sending request to /api/signup');
-        const response = await fetch('https://aaa-fawn-pi.vercel.app/api/signup', {
+        const response = await fetch('https://aaa-eight-rouge.vercel.app/api/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
